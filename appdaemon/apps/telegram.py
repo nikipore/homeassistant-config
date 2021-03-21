e5ca1c13bf0acc90cbe87b5dac17e117d5c9f7d5
Jan Müller <nikipore@mailbox.org>
2021-03-21 18:38:49 +0100

    Complete scaffolding for Telegram bot.

--- appdaemon/apps/telegram.py
+++ appdaemon/apps/telegram.py
@@ -1,13 +1,17 @@
 import appdaemon.plugins.hass.hassapi as hass
 
-SERVICE_SEND = 'telegram_bot/send_message'
-
 class TelegramBot(hass.Hass):
     def initialize(self):
+        self.listen_event(self.receive_telegram_callback, 'telegram_callback')
+        self.listen_event(self.receive_telegram_command, 'telegram_command')
         self.listen_event(self.receive_telegram_text, 'telegram_text')
 
+    def receive_telegram_callback(self, event_id, payload_event, *args):
+        self.log('%s [%s]' % (payload_event, event_id))
+
+    def receive_telegram_command(self, event_id, payload_event, *args):
+        self.log('%s [%s]' % (payload_event, event_id))
+
     def receive_telegram_text(self, event_id, payload_event, *args):
-        self.log(payload_event)
-        text = payload_event['text']
-        user_id = payload_event['user_id']
-        self.call_service(SERVICE_SEND, target=user_id, message='%s -> Pong' % text)
+        self.log('%s [%s]' % (payload_event, event_id))
+        self.call_service('telegram_bot/send_message', target=payload_event['user_id'], message='You wrote: %s' % payload_event['text'])
